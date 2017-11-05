b214dfa853bc0248c4a2f5a946a234e518598b93
Thomas Deconinck <dck@outlook.fr>
2017-11-05 13:55:36 +0100

    fix setup

--- android/app/src/main/java/com/reactnativeandroidtv/MainActivity.java
+++ android/app/src/main/java/com/reactnativeandroidtv/MainActivity.java
@@ -1,8 +1,15 @@
 package com.reactnativeandroidtv;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this); // here
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
--- android/app/src/main/java/com/reactnativeandroidtv/MainApplication.java
+++ android/app/src/main/java/com/reactnativeandroidtv/MainApplication.java
@@ -8,7 +8,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import org.devio.rn.splashscreen.SplashScreen;
 
 import java.util.Arrays;
 import java.util.List;
@@ -42,7 +41,6 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public void onCreate() {
-    SplashScreen.show(this);
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
